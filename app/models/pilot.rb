9e1561f8ac67e339057dee99771ed111299c9f01
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-08-21 11:57:38 +0200

    Add pilot model.

---
+++ app/models/pilot.rb
@@ -0,0 +1,8 @@
+class Pilot < ActiveRecord::Base
+  devise :database_authenticatable, 
+         :registerable,
+         :recoverable, 
+         :rememberable, 
+         :trackable, 
+         :validatable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :pilots
+
   root 'dashboard#index'
 end
