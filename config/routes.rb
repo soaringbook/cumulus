eb7de2b4f660e738109dc9ff135196097e4c7e00
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-08-21 12:28:24 +0200

    Fix the not logged in message.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :pilots
 
-  root 'dashboard#index'
+  authenticated :pilot do
+    # Make sure we don't get the authentication message when we didn't login.
+    root 'dashboard#index', as: :authenticated_root
+  end
+  root :to => redirect('/pilots/sign_in')
 end
