e5011be61dd2d590d0784db1ba3285b99d2ee216
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-08-21 19:14:31 +0200

    Fix the web-icon fonts.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,11 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+# Font configuration.
+#Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts", "web-icons", "less")
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')  
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
 # LESS configuration.
 Rails.application.config.less.paths << File.join(Rails.root, 'vendor', 'less')
 Rails.application.config.less.compress = true
