fea188d62a97b48fd130c9b6b16d997107ec4be4
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-08-26 14:12:27 +0200

    Prepare for language integration.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,14 +4,14 @@
 Rails.application.config.assets.version = '1.0'
 
 # Font configuration.
-#Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts", "web-icons", "less")
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')  
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')  
 Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
 # LESS configuration.
-Rails.application.config.less.paths << File.join(Rails.root, 'vendor', 'less')
+Rails.application.config.less.paths << File.join(Rails.root, 'app', 'assets', 'less')
 Rails.application.config.less.compress = true
 
 # Extra layouts.
 Rails.application.config.assets.precompile += %w( devise.css devise.js )
 Rails.application.config.assets.precompile += %w( errors.css )
+
