10523139cc286c954c8bbee1bbed4830b6abc735
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-08-28 22:26:00 +0200

    Add glider validation.

--- app/models/glider.rb
+++ app/models/glider.rb
@@ -1,2 +1,4 @@
 class Glider < ActiveRecord::Base
+  validates :immatriculation, presence: true, uniqueness: true
+  validates :name,            presence: true
 end
--- spec/models/glider_spec.rb
+++ spec/models/glider_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe Glider do
   context 'Validations' do
+    it { should validate_presence_of(:immatriculation) }
+    it { should validate_uniqueness_of(:immatriculation) }
+    it { should validate_presence_of(:name) }
+
     it 'should have a valid factory' do
       glider = create(:glider)
       expect(glider.valid?).to be_truthy
