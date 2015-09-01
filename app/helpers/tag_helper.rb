a07cdd1deaa413d4a118dff9f0a278a6a7e5112b
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-09-01 08:48:43 +0200

    Add search form.

--- app/helpers/tag_helper.rb
+++ app/helpers/tag_helper.rb
@@ -3,4 +3,14 @@ module TagHelper
     value_class = value ? 'check teal' : 'close red'
     content_tag :i, nil, class: "icon wb-#{value_class}-600"
   end
+
+  def search_form_tag path
+    form_tag path, method: :get do
+      content_tag :div, nil, class: 'input-search input-search-dark' do
+        content = content_tag(:i, nil, class: 'input-search-icon wb-search', aria: { hidden: 'true'})
+        content << text_field_tag(:search, params[:search], placeholder: 'Search', class: 'form-control')
+        content
+      end
+    end
+  end
 end
