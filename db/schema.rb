c8957d36c50f1db1099148de0b193a86a6c4838e
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-09-04 23:32:00 +0200

    Add admin to pilot.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150904211636) do
+ActiveRecord::Schema.define(version: 20150904212640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,21 +50,22 @@ ActiveRecord::Schema.define(version: 20150904211636) do
   add_index "gliders", ["slug"], name: "index_gliders_on_slug", unique: true, using: :btree
 
   create_table "pilots", force: :cascade do |t|
-    t.string   "email",                  default: "",   null: false
-    t.string   "encrypted_password",     default: "",   null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,    null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.integer  "club_id"
-    t.string   "locale",                 default: "en", null: false
-    t.integer  "gliders_access",         default: 0,    null: false
+    t.string   "locale",                 default: "en",  null: false
+    t.integer  "gliders_access",         default: 0,     null: false
+    t.boolean  "admin",                  default: false, null: false
   end
 
   add_index "pilots", ["email"], name: "index_pilots_on_email", unique: true, using: :btree
