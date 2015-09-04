1f36e1e720062bac5b0177c32d578a26a9c98a8a
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-09-05 01:11:08 +0200

    Add rights list feature spec.

---
+++ spec/features/rights_list_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper.rb'
+
+describe 'The rights list flow', type: :feature do
+  before do
+    @pilot = create(:pilot, email: 'brian@fousa.be')
+    login_as(@pilot, scope: :pilot)
+  end
+
+  it 'should see an empty placeholder', js: true do
+    visit rights_path
+
+    fill_in 'Search', with: 'ak'
+    press_enter
+
+    expect(page).to have_content('No pilots found')
+  end
+
+  it 'should see a list with rights' do
+    create(:pilot, club: @pilot.club)
+    visit rights_path
+
+    expect(page).to_not have_content('No pilots found')
+    expect(page).to have_css('tbody tr', count: 2)
+  end
+
+  it 'should only see pilots from my club' do
+    create(:pilot, club: @pilot.club)
+    create(:pilot)
+    visit rights_path
+
+    expect(page).to have_css('tbody tr', count: 2)
+  end
+
+  it 'should be able to search the pilots', js: true do
+    create(:pilot, email: 'jelly@fousa.be', club: @pilot.club)
+    create(:pilot, email: 'jake@fousa.be',  club: @pilot.club)
+    visit rights_path
+
+    fill_in 'Search', with: 'ak'
+    press_enter
+
+    expect(page).to have_css('tbody tr', count: 1)
+  end
+
+  it 'should be able to search the pilots from my club only', js: true do
+    create(:pilot, email: 'jelly@fousa.be', club: @pilot.club)
+    create(:pilot, email: 'jake@fousa.be',  club: @pilot.club)
+    create(:pilot, email: 'jelly2@fousa.be')
+    create(:pilot, email: 'jake2@fousa.be')
+    visit rights_path
+
+    fill_in 'Search', with: 'ak'
+    press_enter
+
+    expect(page).to have_css('tbody tr', count: 1)
+  end
+end
