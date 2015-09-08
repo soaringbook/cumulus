15c25469af121d5d66bd45dd2fe8edaba4cfa935
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-09-08 23:17:27 +0200

    Rubocopyfy the code.

--- app/helpers/tab_helper.rb
+++ app/helpers/tab_helper.rb
@@ -1,5 +1,5 @@
 module TabHelper
-  def tabs_tag &bock
+  def tabs_tag(&_bock)
     content_tag :div, class: 'nav-tabs-horizontal' do
       content_tag :ul, class: 'nav nav-tabs nav-tabs-line', data: { plugin: 'nav-tabs' }, role: 'tablist' do
         yield
@@ -7,7 +7,7 @@ module TabHelper
     end
   end
 
-  def tab_link_to title, icon, content_id, active = false
+  def tab_link_to(title, icon, content_id, active = false)
     active_class = active ? 'active' : nil
     content_tag :li, role: 'presentation', class: active_class do
       link_to "##{content_id}", data: { toggle: 'tab', url: content_id }, aria: { controls: content_id, expanded: active } do
@@ -18,13 +18,13 @@ module TabHelper
     end
   end
 
-  def tab_content &block
+  def tab_content(&_block)
     content_tag :div, class: 'tab-content padding-top-20' do
       yield
     end
   end
 
-  def tab_pane content_id, active = false, &block
+  def tab_pane(content_id, active = false, &_block)
     active_class = active ? ' active' : ''
     content_tag :div, id: content_id, class: "tab-pane #{active_class}", role: 'tabpanel' do
       yield
