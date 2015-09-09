ca0665d7b254a02ccb3478b86e5a3f372de5f7e2
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-09-09 13:08:12 +0200

    Fix the registration flow specs.

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -1,23 +1,25 @@
 require 'rails_helper.rb'
 
 describe 'The registration flow', type: :feature do
-  before { show_registration_path }
+  before do
+    clear_emails
+    show_registration_path
+  end
 
   it 'should register a user' do
     fill_in 'Club abbreviation (ex. BZC)', with: 'ebbt'
-    fill_in 'First name',                  with: 'Jake'
-    fill_in 'Last name',                   with: 'The Snake'
     fill_in 'Email',                       with: 'jake@snake.be'
-    fill_in 'Password',                    with: '123123123', match: :prefer_exact
-    fill_in 'Confirm password',            with: '123123123', match: :prefer_exact
     click_button 'Create yours'
 
-    expect(page).to have_content('Soaring Book')
+    expect(page).to have_content('confirmation link')
 
     expect(Pilot.count).to eq(1)
     expect(Club.count).to eq(1)
     expect(Pilot.first.club.id).to eq(Club.first.id)
     expect(Pilot.first.admin?).to be_truthy
+
+    open_email('jake@snake.be')
+    expect(current_email).to have_content 'Confirm'
   end
 
   it 'should fail to register a user' do
@@ -31,8 +33,6 @@ describe 'The registration flow', type: :feature do
 
   it 'should fail to register a user when the club is invalid' do
     fill_in 'Email',            with: 'jake@snake.be'
-    fill_in 'Password',         with: '123123123', match: :prefer_exact
-    fill_in 'Confirm password', with: '123123123', match: :prefer_exact
     click_button 'Create yours'
 
     expect(page).to have_content("can't be blank")
