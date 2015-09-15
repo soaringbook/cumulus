346500f25ba1052bf6af784530dbfc82bae13ed4
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-09-15 23:55:19 +0200

    Fix rubocop.

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,11 +2,11 @@ class Plan < ActiveRecord::Base
   include Payola::Plan
 
   has_many :clubs
-  has_many :subscriptions, class_name: "Payola::Subscription", dependent: :destroy
+  has_many :subscriptions, class_name: 'Payola::Subscription', dependent: :destroy
 
-  validates :stripe_id, inclusion: { in: Plan.pluck('DISTINCT stripe_id'), message: "not a valid subscription plan" }
+  validates :stripe_id, inclusion: { in: Plan.pluck('DISTINCT stripe_id'), message: 'not a valid subscription plan' }
 
-  def redirect_path(subscription)
+  def redirect_path(_subscription)
     routes = Rails.application.routes.url_helpers
     routes.edit_account_path
   end
@@ -20,10 +20,10 @@ class Plan < ActiveRecord::Base
   ### Plans
 
   def self.cumulus
-    Plan.where(stripe_id: 'cumulus').first
+    Plan.find_by(stripe_id: 'cumulus')
   end
 
   def self.cumulus_no_trial
-    Plan.where(stripe_id: 'cumulus_no_trial').first
+    Plan.find_by(stripe_id: 'cumulus_no_trial')
   end
 end
