a96a52db014502cb5f40f87b62f9d52fbfb62b4b
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-09-15 23:47:23 +0200

    Make sure most test success with subscriptions.

--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -3,6 +3,7 @@ module ControllerHelpers
     @pilot = pilot
     allow(request.env['warden']).to receive(:authenticate!) { pilot }
     allow(controller).to receive(:current_pilot) { pilot }
+    allow(controller).to receive(:current_subscription) { build(:subscription) }
   end
 
   def redirect_for_login(&_block)
