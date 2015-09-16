c9e1ad8339872f1ce45bc0933ad6ff0100921b75
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-09-16 14:50:39 +0200

    Generate a cumulus plan from a rake task.

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,8 +4,6 @@ class Plan < ActiveRecord::Base
   has_many :clubs
   has_many :subscriptions, class_name: 'Payola::Subscription', dependent: :destroy
 
-  validates :stripe_id, inclusion: { in: Plan.pluck('DISTINCT stripe_id'), message: 'not a valid subscription plan' }
-
   def redirect_path(_subscription)
     routes = Rails.application.routes.url_helpers
     routes.edit_account_path
@@ -20,26 +18,6 @@ class Plan < ActiveRecord::Base
   ### Plans
 
   def self.cumulus
-    @cumulus || begin
-      cumulus = Plan.where(stripe_id: 'cumulus').first_or_initialize do |plan|
-        plan.amount = 3000
-        plan.interval = 'month'
-        plan.name = 'Cumulus'
-      end
-      cumulus.save!(validate: false)
-      cumulus
-    end
-  end
-
-  def self.cumulus_no_trial
-    @cumulus_no_trial || begin
-      cumulus = Plan.where(stripe_id: 'cumulus_no_trial').first_or_initialize do |plan|
-        plan.amount = 3000
-        plan.interval = 'month'
-        plan.name = 'Cumulus no trial'
-      end
-      cumulus.save!(validate: false)
-      cumulus
-    end
+    Plan.where(stripe_id: 'cumulus').first
   end
 end
