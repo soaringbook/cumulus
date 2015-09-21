0009cf70df6ba7c3ec02358fc7bbeb9748db19c3
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-09-21 12:47:14 +0200

    Change url when switching tab.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,12 @@
 //= require jquery-cookie/jquery.cookie.js
 //= require select2/select2.full.min.js
 //= require inspinia.js
+//= require url_handling
 //= require retina_tag
+
+$(function() {
+  // Change the url when clicking a tab.
+  $('a[data-toggle=tab]').on('click', function() {
+    changeUrlParam('tab', $(this).data('url'));
+  });
+});
