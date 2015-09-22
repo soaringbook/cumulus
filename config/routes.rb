3b0498206235f36667db2eb0d04bdf6bcb538023
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-09-22 12:56:56 +0200

    Better dashboard routing.

--- config/routes.rb
+++ config/routes.rb
@@ -19,13 +19,6 @@ Rails.application.routes.draw do
                             as: :pilot_registration
   end
 
-  ### Dashboard
-
-  authenticated :pilot do
-    # Make sure we don't get the authentication message when we didn't login.
-    root 'dashboard#show', as: :authenticated_root
-  end
-
   ### Stripe confirmation
 
   resource :payments, only: [:show]
@@ -60,5 +53,5 @@ Rails.application.routes.draw do
 
   ### Root
 
-  root to: redirect('/pilots/sign_in')
+  root to: 'dashboard#show'
 end
