3d1b6d7aae0e2f5ed3f47c3d2e357681d624b40f
Jelle Vandebeeck <jelle@fousa.be>
2016-02-22 19:01:13 +0100

    Cleanup the glider/pilot import controllers.

--- app/controllers/import/gliders_controller.rb
+++ app/controllers/import/gliders_controller.rb
@@ -1,47 +1,7 @@
 module Import
-  class GlidersController < ApplicationController
-    include Wicked::Wizard
-
-    before_action :authorize_resource!
-
-    steps :upload,
-          :review,
-          :result
-
-    def show
-      @import_object = ImportObject.new
-      render(:upload)
-    end
-
-    def update
-      case step
-      when :review
-        handle_upload
-      when :result
-        handle_result
-      end
-    end
-
-    private
-
-    def authorize_resource!
-      authorize! :create, Glider
-    end
-
-    def handle_upload
-      service = ImportService.new(Glider, params[:import_object])
-      if service.validate_for current_club
-        @valid_gliders, @invalid_gliders = service.records
-        render(:review)
-      else
-        @import_object = service.object
-        render(:upload)
-      end
-    end
-
-    def handle_result
-      @gliders, @failed_gliders = Glider.import(params[:review][:records], current_club)
-      render(:result)
+  class GlidersController < BaseController
+    def resource_class
+      Glider
     end
   end
 end
--- app/controllers/import/pilots_controller.rb
+++ app/controllers/import/pilots_controller.rb
@@ -1,47 +1,7 @@
 module Import
-  class PilotsController < ApplicationController
-    include Wicked::Wizard
-
-    before_action :authorize_resource!
-
-    steps :upload,
-          :review,
-          :result
-
-    def show
-      @import_object = ImportObject.new
-      render(:upload)
-    end
-
-    def update
-      case step
-      when :review
-        handle_upload
-      when :result
-        handle_result
-      end
-    end
-
-    private
-
-    def authorize_resource!
-      authorize! :create, Pilot
-    end
-
-    def handle_upload
-      service = ImportService.new(Pilot, params[:import_object])
-      if service.validate_for current_club
-        @valid_pilots, @invalid_pilots = service.records
-        render(:review)
-      else
-        @import_object = service.object
-        render(:upload)
-      end
-    end
-
-    def handle_result
-      @pilots, @failed_pilots = Pilot.import(params[:review][:records], current_club)
-      render(:result)
+  class PilotsController < BaseController
+    def resource_class
+      Pilot
     end
   end
 end
