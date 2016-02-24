7d6d83c480b92bc7059c6d397712061595b3d945
Jelle Vandebeeck <jelle@fousa.be>
2016-02-24 23:22:57 +0100

    Add the club feature specs.

---
+++ spec/features/clubs/club_edit_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper.rb'
+
+describe 'The club edit flow', type: :feature do
+  before { login_as_pilot create(:pilot, super_admin: true) }
+
+  it 'should create a new club' do
+    visit new_club_path
+
+    fill_in 'Abbreviation', with: 'aaa'
+    click_button 'Add Club'
+
+    expect(page).to have_content('Club was successfully created')
+    expect(Club.count).to eq(2)
+  end
+
+  it 'should fail to create a new club' do
+    visit new_club_path
+
+    click_button 'Add Club'
+
+    expect(page).to have_content('can\'t be blank')
+    expect(Club.count).to eq(1)
+  end
+
+  it 'should edit a club' do
+    club = create(:club)
+    visit edit_club_path(club)
+
+    fill_in 'Abbreviation', with: 'bbb'
+    click_button 'Update Club'
+
+    expect(page).to have_content('Club was successfully updated')
+  end
+
+  it 'should fail to edit a pilot' do
+    club = create(:club)
+    visit edit_club_path(club)
+
+    fill_in 'Abbreviation', with: ''
+    click_button 'Update Club'
+
+    expect(page).to have_content('can\'t be blank')
+  end
+end
---
+++ spec/features/clubs/club_list_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper.rb'
+
+describe 'The club list flow', type: :feature do
+  before { login_as_pilot create(:pilot, super_admin: true) }
+
+  it 'should see a list with clubs' do
+    create(:club)
+    visit clubs_path
+
+    expect(page).to_not have_content('No clubs found')
+    expect(page).to have_css('tbody tr', count: 2)
+  end
+
+  it 'should be able to search the clubs', js: true do
+    create(:club, short_name: 'abcd')
+    create(:club, short_name: 'abd')
+    visit clubs_path
+
+    fill_in 'Search for clubs...', with: 'bc'
+    press_enter '#search'
+
+    expect(page).to have_css('tbody tr', count: 1)
+  end
+
+  it 'should be able to search the clubs', js: true do
+    create(:club, short_name: 'Nam')
+    create(:club, short_name: 'Noo')
+    create(:club, short_name: 'Nama')
+    create(:club, short_name: 'Noa')
+    visit clubs_path
+
+    fill_in 'Search for clubs...', with: 'NA'
+    press_enter '#search'
+
+    expect(page).to have_css('tbody tr', count: 1)
+  end
+end
