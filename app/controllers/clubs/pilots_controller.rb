3902ca6aec2eb2d82d40bbd2a0105eb13fc67433
Jelle Vandebeeck <jelle@fousa.be>
2016-02-28 19:36:26 +0100

    Submit the invite creation.

--- app/controllers/clubs/pilots_controller.rb
+++ app/controllers/clubs/pilots_controller.rb
@@ -11,11 +11,18 @@ class Clubs::PilotsController < ApplicationController
   end
 
   def create
+    @pilot = current_club.pilots.build(pilot_params)
+    @pilot.admin = true
+    @pilot.save
   end
 
   private
 
   def set_club
     @club = Club.friendly.find(params[:club_id])
+  end
+
+  def pilot_params
+    params.require(:pilot).permit(:email, :first_name, :last_name)
   end
 end
