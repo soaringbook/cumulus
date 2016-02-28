d1fda7cc0d08c4895f0fe56ff393953f271a86b5
Jelle Vandebeeck <jelle@fousa.be>
2016-02-28 10:14:58 +0100

    Refactor the current container for reuse.

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -1,9 +1,22 @@
 module ModalHelper
   def modal_button_tag(identifier, title)
-    content_tag :button, title, class: 'btn btn-white', data: { target: "##{identifier}", toggle: 'modal' }
+    link_to title, '#', class: 'btn btn-white', data: { target: "##{identifier}", toggle: 'modal' }
   end
 
   def modal_container(identifier, icon, title, subtitle, &_block)
+    modal_raw_container identifier, icon, title, subtitle do
+      content = content_tag :div, class: 'modal-body' do
+        yield if block_given?
+      end
+      footer = content_tag :div, class: 'modal-footer' do
+        content_tag :button, t('global.links.close'), class: 'btn btn-white', data: { dismiss: 'modal' }
+      end
+      content << footer
+      content
+    end
+  end
+
+  def modal_raw_container(identifier, icon, title, subtitle, &_block)
     content_tag :div, id: identifier, class: 'modal inmodal', role: 'dialog', tabindex: '-1' do
       content_tag :div, class: 'modal-dialog' do
         content_tag :div, class: 'modal-content animated bounceInRight' do
@@ -13,16 +26,7 @@ module ModalHelper
             inner_content << content_tag(:small, subtitle, class: 'small font-bold')
             inner_content
           end
-          if block_given?
-            body = content_tag :div, class: 'modal-body' do
-              yield
-            end
-            content << body
-          end
-          footer = content_tag :div, class: 'modal-footer' do
-            content_tag :button, t('global.links.close'), class: 'btn btn-white', data: { dismiss: 'modal' }
-          end
-          content << footer
+          content << yield if block_given?
           content
         end
       end
