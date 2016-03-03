3296de5595dc481ea606c843ab5f21d72e0bf234
Jelle Vandebeeck <jelle@fousa.be>
2016-03-03 16:12:10 +0100

    Add the invite form spec.

--- spec/features/clubs/pilots/invite_spec.rb
+++ spec/features/clubs/pilots/invite_spec.rb
@@ -4,21 +4,17 @@ describe 'The club pilots invite flow', type: :feature do
   before do
     clear_emails
     login_as_pilot create(:pilot, super_admin: true)
-  end
 
-  # it 'should see the invite form', js: true do
-  #   club = create(:club)
-  #   visit edit_club_path(club)
-  #   click_link 'Invite pilot'
-  #
-  #   expect(page).to have_content('Invite a pilot by filling')
-  # end
-
-  it 'should successfully invite a pilot', js: true do
     club = create(:club)
     visit edit_club_path(club)
     click_link 'Invite pilot'
+  end
 
+  it 'should see the invite form', js: true do
+    expect(page).to have_content('First name')
+  end
+
+  it 'should successfully invite a pilot', js: true do
     fill_in 'Email', with: 'jake@snake.be'
     fill_in 'First name', with: 'Jake'
     fill_in 'Last name', with: 'Snake'
@@ -29,17 +25,13 @@ describe 'The club pilots invite flow', type: :feature do
     expect(current_email).to have_content 'Confirm'
   end
 
-  # it 'should fail to invite a pilot', js: true do
-  #   club = create(:club)
-  #   visit edit_club_path(club)
-  #   click_link 'Invite pilot'
-  #
-  #   fill_in 'Email', with: 'jake@snake.be'
-  #   click_button 'Invite pilot'
-  #
-  #   sleep 1 # Fix for triggering email from JS.
-  #   open_email('jake@snake.be')
-  #   expect(current_email).to be_nil
-  #   expect(page).to have_content("can't be blank")
-  # end
+  it 'should fail to invite a pilot', js: true do
+    fill_in 'Email', with: 'jake@snake.be'
+    click_button 'Invite pilot'
+
+    sleep 1 # Fix for triggering email from JS.
+    open_email('jake@snake.be')
+    expect(current_email).to be_nil
+    expect(page).to have_content("can't be blank")
+  end
 end
